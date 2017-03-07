bc841c4625065a20773cc2af629851310719b274
Gabriel de Souza <devgabrieldesouza@gmail.com>
2017-03-07 19:40:09 -0300

    Exercises TP1 - 3

---
+++ TP1/exe3_1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "" << 4 * 18 - 198 % 10 << endl;
+	return 0;
+}
---
+++ TP1/exe3_4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a, b , c, d = 123;
+	a = d;
+	b = c = d;
+	
+	cout << "Valor de a é: " << a << endl;
+	cout << "Valor de b é: " << b << endl;
+	cout << "Valor de c é: " << c << endl;
+	cout << "Valor de d é: " << d << endl;
+	
+	return 0;
+}
