af3f827f3bc6d59ced0ca876f653803e649980d6
Gabriel de Souza <devgabrieldesouza@gmail.com>
2017-03-07 19:44:11 -0300

    Add files via upload

---
+++ TP1/exe3_6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a, b;
+	a = 10;
+	a = a - 3;
+	b = 10;
+	b-= 3;
+	
+	cout << "Valor de a é: " << a << endl;
+	cout << "Valor de b é: " << b << endl;
+
+	return 0;
+}
---
+++ TP1/exe3_7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a;
+	double b = 3.33;
+	a = b;
+	
+	cout << "Valor de a é: " << a << endl;
+	cout << "Valor de b é: " << b << endl;
+
+	return 0;
+}
